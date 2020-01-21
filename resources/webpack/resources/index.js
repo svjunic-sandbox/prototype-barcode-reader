4be090eb469b7fdbc579ddaaf9d1d893a44826e6
svjunic <sv.junic@gmail.com>
2020-01-21 11:34:58 +0900

    delete no use file

--- resources/webpack/resources/index.js
+++ resources/webpack/resources/index.js
@@ -1,1 +1,22 @@
-console.log('mogeta');
+import Quagga from 'quagga';
+
+Quagga.init(
+  {
+    inputStream: {
+      name: 'Live',
+      type: 'LiveStream',
+      target: document.querySelector('#live') // Or '#yourElement' (optional)
+    },
+    decoder: {
+      readers: ['code_128_reader']
+    }
+  },
+  function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Initialization finished. Ready to start');
+    Quagga.start();
+  }
+);
