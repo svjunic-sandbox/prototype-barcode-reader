01058e6a4d32524f9b446f67d671226127f4fb7d
svjunic <sv.junic@gmail.com>
2020-01-21 11:20:58 +0900

    add resources

---
+++ resources/webpack/webpack.config.js
@@ -0,0 +1,96 @@
+console.log(process.env.NODE_ENV);
+
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const DOCUMENT_ROOT = '../../docs/';
+
+module.exports = function() {
+  const entries = {
+    'common/js/index': path.join(__dirname, './resources/index.js')
+  };
+
+  const watchOptions = {
+    poll: 1000
+  };
+
+  const output = {
+    path: path.join(__dirname, DOCUMENT_ROOT),
+    filename: '[name].js'
+    //jsonpFunction: 'svjunic'
+  };
+
+  let optimization = {
+    splitChunks: {
+      name: 'common/js/vendor',
+      chunks: 'initial'
+    }
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    optimization = Object.assign(optimization, {
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            warnings: false,
+            compress: {
+              drop_console: true
+            }
+          }
+        })
+      ]
+    });
+  }
+
+  const resolve = {
+    modules: [path.resolve(__dirname, 'resources'), 'node_modules']
+  };
+
+  const baseConfig = {
+    target: 'web',
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader'
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'html-loader'
+            }
+          ]
+        }
+      ]
+    },
+
+    plugins: [new webpack.NoEmitOnErrorsPlugin(), new webpack.optimize.AggressiveMergingPlugin()]
+  };
+
+  return [
+    Object.assign(
+      {
+        mode: 'production',
+        watchOptions: watchOptions,
+        entry: entries,
+        resolve: resolve,
+        output: output,
+        optimization: optimization
+      },
+      baseConfig
+    )
+  ];
+};
